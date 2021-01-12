ddd0da969cce1423172d9c12f124e0b851a69832
Ashirvad Jena <ashirvad.aj@yahoo.com>
2021-01-12 21:58:54 +0530

    added constant variables for static strings

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Library Code
 function createStore(reducer) {
-	// It consists of 4 parts
+	// The store should have four parts
 	// 1. The state
-	// 2. Getting the state
-	// 3. Listening to state
-	// 4. updating the state
+	// 2. Get the state.
+	// 3. Listen to changes on the state.
+	// 4. Update the state
 
 	let state;
 	let listeners = [];
@@ -23,40 +23,43 @@ function createStore(reducer) {
 		listeners.forEach((listener) => listener());
 	};
 
-	return { getState, subscribe, dispatch };
+	return {
+		getState,
+		subscribe,
+		dispatch,
+	};
 }
 
 // App Code
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
 function todos(state = [], action) {
-	console.log(state);
 	switch (action.type) {
-		case "ADD_ITEM":
+		case ADD_TODO:
 			return state.concat([action.todo]);
-
-		case "DELETE_ITEM":
-			return state.filter((todo) => todo.id !== action.todo.id);
-
-		case "TOGGLE_ITEM":
+		case REMOVE_TODO:
+			return state.filter((todo) => todo.id !== action.id);
+		case TOGGLE_TODO:
 			return state.map((todo) =>
-				todo.id !== action.todo.id
+				todo.id !== action.id
 					? todo
-					: Object.assign({}, todo, { completed: !todo.id })
+					: Object.assign({}, todo, { complete: !todo.complete })
 			);
-
 		default:
 			return state;
 	}
 }
 
 function goals(state = [], action) {
-	console.log(state);
 	switch (action.type) {
-		case "ADD_GOAL":
-			return state.concat([action.todo]);
-
-		case "DELETE_GOAL":
-			return state.filter((goal) => goal.id !== action.goal.id);
-
+		case ADD_GOAL:
+			return state.concat([action.goal]);
+		case REMOVE_GOAL:
+			return state.filter((goal) => goal.id !== action.id);
 		default:
 			return state;
 	}
@@ -72,18 +75,63 @@ function app(state = {}, action) {
 const store = createStore(app);
 
 store.subscribe(() => {
-	console.log("the updated State is:", store.getState());
+	console.log("The new state is: ", store.getState());
 });
 
 store.dispatch({
-	type: "ADD_ITEM",
-	todo: { id: 0, name: "Learning redux", status: true },
+	type: ADD_TODO,
+	todo: {
+		id: 0,
+		name: "Walk the dog",
+		complete: false,
+	},
 });
+
 store.dispatch({
-	type: "ADD_ITEM",
-	todo: { id: 2, name: "Learning dispatch", completed: false },
+	type: ADD_TODO,
+	todo: {
+		id: 1,
+		name: "Wash the car",
+		complete: false,
+	},
 });
+
 store.dispatch({
-	type: "DELETE_ITEM",
-	todo: { id: 0, name: "Learning redux", status: true },
+	type: ADD_TODO,
+	todo: {
+		id: 2,
+		name: "Go to the gym",
+		complete: true,
+	},
+});
+
+store.dispatch({
+	type: REMOVE_TODO,
+	id: 1,
+});
+
+store.dispatch({
+	type: TOGGLE_TODO,
+	id: 0,
+});
+
+store.dispatch({
+	type: ADD_GOAL,
+	goal: {
+		id: 0,
+		name: "Learn Redux",
+	},
+});
+
+store.dispatch({
+	type: ADD_GOAL,
+	goal: {
+		id: 1,
+		name: "Lose 20 pounds",
+	},
+});
+
+store.dispatch({
+	type: REMOVE_GOAL,
+	id: 0,
 });
