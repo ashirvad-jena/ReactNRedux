44b62ad363ed11d5155217019b63066e0850f5bb
Ashirvad Jena <>
2021-01-12 15:17:52 +0530

    listening to changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use-strict";
 
-function createState() {
+function createStore() {
 	// It consists of 4 parts
 	// 1. The state
 	// 2. Getting the state
@@ -8,11 +8,33 @@ function createState() {
 	// 4. updating the state
 
 	let state = { hello: "hi" };
+	let listeners = [];
 
 	const getState = () => state;
 
-	return { getState };
+	const subscribe = (listener) => {
+		listeners.push(listener);
+		return () => {
+			listeners = listeners.filter((l) => l !== listener);
+			console.log(listeners.length);
+		};
+	};
+
+	return { getState, subscribe, listeners };
 }
 
-// x = createState();
-// console.log(x.getState());
+// const store = createStore();
+// store.subscribe(() => {
+// 	console.log(`The updated state is ${store.getState()}`);
+// });
+// const secondSubscribtion = store.subscribe(() => {
+// 	console.log(`The updated state 2 is ${store.getState()}`);
+// });
+
+// secondSubscribtion();
+
+// console.log(store.listeners.length);
+
+// store.listeners.forEach((element) => {
+// 	element();
+// });
